617400434f81a226c3f6913aa3848ae9ff3fc371
ChanasornHalcyon <chanasorn@halcyon.co.th>
2025-10-26 14:00:23 +0700

    WIP

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
     origin: ["https://halcyon-one-internal.vercel.app"],
