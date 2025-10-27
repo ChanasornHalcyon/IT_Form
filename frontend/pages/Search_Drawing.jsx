c17e6be8d3e2ca2d08b2046f4474a75d79d5e705
ChanasornHalcyon <chanasorn@halcyon.co.th>
2025-10-27 08:42:19 +0700

    Delete Required

--- frontend/pages/Search_Drawing.jsx
+++ frontend/pages/Search_Drawing.jsx
@@ -71,7 +71,6 @@ const Search_Drawing = () => {
                 name="customerName"
                 value={form.customerName}
                 onChange={handleChange}
-                required
                 className="block w-full border border-gray-400 rounded-md px-3 py-2 focus:border-[#0B4EA2] text-black"
               />
             </div>
@@ -86,7 +85,6 @@ const Search_Drawing = () => {
                 value={form.date}
                 onChange={handleChange}
                 onClick={(e) => e.target.showPicker()}
-                required
                 className="block w-full border border-gray-400 rounded-md px-3 py-2 focus:border-[#0B4EA2] text-gray-700 cursor-pointer"
               />
             </div>
